8e01d3344c15a7fab41d91c4ae4880f711b95e9a
dinislamweb <96992849+dinislamweb@users.noreply.github.com>
2026-06-11 22:44:11 +0600

    Django REST API practice: FBV GET and serializers

--- api/views.py
+++ api/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+#from django.shortcuts import render
+#from django.http import JsonResponse
+from students.models import Student
+from .serializers import StudentSerialier
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
+@api_view(['GET'])
 def studentsView(request):
-    students = [
-        {
-            'id':1,
-            'name':'Ratan',
-            'class':'Computer Science'
-        }
-    ]
-    return JsonResponse({'students': students})
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerialier(students,many=True)
+    
+    return Response(serializer.data,status=status.HTTP_200_OK)
