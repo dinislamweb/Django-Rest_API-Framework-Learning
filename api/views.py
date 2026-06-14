f36cfd0116d4d6251ced933f2bbece0dff20e519
dinislamweb <96992849+dinislamweb@users.noreply.github.com>
2026-06-14 20:52:28 +0600

    retrieve data using primary key by using class based view

--- api/views.py
+++ api/views.py
@@ -64,6 +64,18 @@ class Employees(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+class EmployeeDetail(APIView):
+    def get_object(self,pk):
+        try:
+            return Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            raise Http404
+    
+    # GET → Retrieve a single employee
+    def get(self, request, pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
         
