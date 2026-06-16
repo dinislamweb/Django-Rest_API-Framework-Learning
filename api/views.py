51c72d3f277d53b0168e46af20298277dd52b128
dinislamweb <96992849+dinislamweb@users.noreply.github.com>
2026-06-16 20:16:01 +0600

    Update  objects based on primary key using generics.UpdateAPIView

--- api/views.py
+++ api/views.py
@@ -134,7 +134,7 @@ class Employees(generics.ListCreateAPIView):
     serializer_class = EmployeeSerializer
 
 # Primary key based operations 
-class EmployeeDetail(generics.RetrieveAPIView):
+class EmployeeDetail(generics.RetrieveAPIView,generics.UpdateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     lookup_field = 'pk'
